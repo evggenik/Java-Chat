076458fa437cdfb0d25cb9347d91467c0d20eba9
Evgeny Buchelnikov <gru554@yandex.ru>
2025-12-16 20:43:44 +1000

    added static method for sending messages to all clients at once

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,9 +1,15 @@
 package main.java;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         int serverPort = ConsoleHelper.readInt();
         try(ServerSocket serverSocket = new ServerSocket(serverPort)) {
@@ -14,6 +20,16 @@ public class Server {
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> entry: connectionMap.entrySet()) {
+            try {
+                entry.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println("The message can not be sent");
+            }
         }
     }
 
