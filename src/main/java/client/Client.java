9f66b193b8c93a6072c81a7779ed833d6345db0a
Evgeny Buchelnikov <gru554@yandex.ru>
2025-12-26 20:01:31 +1000

    added Client class

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,13 @@
+package main.java.client;
+
+import main.java.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread {
+
+    }
+
+}
