ba10125443df274507ac86ce0684ae512748b733
Evgeny Buchelnikov <gru554@yandex.ru>
2025-12-30 14:10:09 +1000

    overrode some methods in BotSocketThread to make the bot to accept some commands

--- src/main/java/client/BotClient.java
+++ src/main/java/client/BotClient.java
@@ -1,5 +1,8 @@
 package main.java.client;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class BotClient extends Client {
@@ -11,6 +14,43 @@ public class BotClient extends Client {
     }
 
     public class BotSocketThread extends SocketThread {
+
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Hello chat. I am bot. I accept these commands: " +
+                    "date, day, month, year, time, hour, minutes, seconds.");
+            super.clientMainLoop();
+        }
+
+        @Override
+        protected void processIncomingMessage(String message) {
+            super.processIncomingMessage(message);
+            if (!message.contains(":")) return;
+            String[] parts = message.split(":", 2);
+            if (parts.length != 2) return;
+
+            String userName = parts[0];
+            String text = parts[1].trim();
+
+            String pattern;
+
+            switch (text) {
+                case "дата" -> pattern = "d.MM.YYYY";
+                case "день" -> pattern = "d";
+                case "месяц" -> pattern = "MMMM";
+                case "год" -> pattern = "YYYY";
+                case "время" -> pattern = "H:mm:ss";
+                case "час" -> pattern = "H";
+                case "минуты" -> pattern = "m";
+                case "секунды" -> pattern = "s";
+                default -> { return; }
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            String result = sdf.format(Calendar.getInstance().getTime());
+            sendTextMessage(
+                    "Информация для " + userName + ": " + result
+            );
+        }
 
     }
 
